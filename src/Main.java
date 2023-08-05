c279f27f378ba9e5df60ecc8942fe1ed35036df2
dgill45 <donell.gill@outlook.com>
2023-08-05 16:10:33 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner scan = new Scanner(System.in);
+    int choice = scan.nextInt();
+
+    double temperature;
+    double convertedTemperature;
+}

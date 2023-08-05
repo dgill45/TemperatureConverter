d445d8a5ac34a332ec33a2fbd562fb78b5ead922
dgill45 <donell.gill@outlook.com>
2023-08-05 16:15:51 -0400

    Ask user to enter choice of Celcius or Fahrenheit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    Scanner scan = new Scanner(System.in);
-    int choice = scan.nextInt();
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Temperature Converter");
+        System.out.println("1. Celsius to Fahrenheit");
+        System.out.println("2. Fahrenheit to Celsius");
+        System.out.print("Enter your choice (1/2): ");
+        int choice = scan.nextInt();
 
-    double temperature;
-    double convertedTemperature;
+        double temperature;
+        double convertedTemperature;
+    }
 }

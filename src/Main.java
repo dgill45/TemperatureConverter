58f34f15a493f55fb7ee05225ec505d1dcab8322
dgill45 <donell.gill@outlook.com>
2023-08-05 16:20:02 -0400

    Added switch case to handle user choice

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,26 @@ public class Main {
 
         double temperature;
         double convertedTemperature;
+
+        switch (choice) {
+            case 1:
+                System.out.print("Enter temperature in Celsius: ");
+                temperature = scan.nextDouble();
+                convertedTemperature = TempConverter.celsiusToFahrenheit(temperature);
+                System.out.println("Converted temperature in Fahrenheit: " + convertedTemperature + " °F");
+                break;
+
+            case 2:
+                System.out.print("Enter temperature in Fahrenheit: ");
+                temperature = scan.nextDouble();
+                convertedTemperature = TempConverter.fahrenheitToCelsius(temperature);
+                System.out.println("Converted temperature in Celsius: " + convertedTemperature + " °C");
+                break;
+
+            default:
+                System.out.println("Invalid choice. Please select 1 or 2.");
+        }
+
+        scan.close();
     }
 }
